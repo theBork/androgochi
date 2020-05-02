6d2948b6373ddfb4281886143cb455dcefb5b0eb
Mikhail Kutnyashenko <mikhail@effective.band>
2020-05-02 19:30:29 +0300

    Fixed port env

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`);
 })
